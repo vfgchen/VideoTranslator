336efbe756ef3b0301408249bb4f699f931c5da3
vfgchen <vfgchen@163.com>
2025-10-14 19:43:04 +0800

    txt to req

--- task_txt_to_req.py
+++ task_txt_to_req.py
@@ -1,4 +1,5 @@
 import asyncio
+import argparse
 
 from util import *
 from audio import *
@@ -7,8 +8,14 @@ from subtitle import *
 from openai_deepseek import *
 
 async def main():
-    files = list_files(subtitle_dir, ".txt")
-    await async_batch_exec(files, txt_to_req, "Power Platform PL-100")
+    parser = argparse.ArgumentParser(description="*.txt to *.req")
+    parser.add_argument("--subtitle_dir", help="subtitle directory", default=f"{subtitle_dir}")
+    parser.add_argument("--suffix", help="txt filename suffix", default=".txt")
+    parser.add_argument("--topic", help="topic", default="Power Platform PL-100")
+    args = parser.parse_args()
+
+    files = list_files(args.subtitle_dir, args.suffix)
+    await async_batch_exec(files, txt_to_req, args.topic)
 
 if __name__ == "__main__":
     asyncio.run(main())
