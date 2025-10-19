7b75578945b41684287e5ee2b7d59f11daf01293
vfgchen <vfgchen@163.com>
2025-10-19 22:25:49 +0800

    ai prompt

--- task_res_check.py
+++ task_res_check.py
@@ -9,7 +9,7 @@ async def main():
     parser = argparse.ArgumentParser(description="*.res check")
     parser.add_argument("--subtitle_dir", help="subtitle directory", default="subtitles")
     parser.add_argument("--suffix", help="res filename suffix", default=".res")
-    parser.add_argument("--topic", help="topic", default="Power Platform PL-400")
+    parser.add_argument("--topic", help="topic", default="Dynamics 365 F&O SCM")
     args = parser.parse_args()
 
     subtitle_dir=args.subtitle_dir
