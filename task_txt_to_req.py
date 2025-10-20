fdf6d7b77560bd07de448d389c54c91be7e85069
vfgchen <vfgchen@163.com>
2025-10-20 23:04:00 +0800

    topic: Dynamics 365 F&O SCM

--- task_txt_to_req.py
+++ task_txt_to_req.py
@@ -9,7 +9,7 @@ async def main():
     parser = argparse.ArgumentParser(description="*.txt to *.req")
     parser.add_argument("--subtitle_dir", help="subtitle directory", default="subtitles")
     parser.add_argument("--suffix", help="txt filename suffix", default=".txt")
-    parser.add_argument("--topic", help="topic", default="Power Platform PL-200")
+    parser.add_argument("--topic", help="topic", default="Dynamics 365 F&O SCM")
     args = parser.parse_args()
 
     subtitle_dir=args.subtitle_dir
